f6cb95a1741c0d3d6d033ea587798773605faa25
cthier <cthier@56eb1bfb-7d52-0410-98ca-1538ac455fc3>
2008-07-24 04:43:02 +0000

    Adding support for installable HTML/CSS auto-complete

---
+++ pylib/lang_mako.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# Copyright (c) 2008 Chuck Thier
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in
+#    all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+#    USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""Mako support for codeintel.
+
+This file will be imported by the codeintel system on startup and the
+register() function called to register this language with the system. All
+Code Intelligence for this language is controlled through this module.
+"""
+
+import os
+import sys
+import logging
+
+from codeintel2.common import *
+from codeintel2.langintel import LangIntel
+from codeintel2.udl import UDLLexer, UDLBuffer, UDLCILEDriver, XMLParsingBufferMixin
+
+try:
+    from xpcom.server import UnwrapObject
+    _xpcom_ = True
+except ImportError:
+    _xpcom_ = False
+
+#---- globals
+
+lang = "Mako"
+log = logging.getLogger("codeintel.mako")
+#log.setLevel(logging.DEBUG)
+
+#---- Lexer class
+
+class MakoLexer(UDLLexer):
+    lang = lang
+
+#---- LangIntel class
+
+# Dev Notes:
+# All language should define a LangIntel class. (In some rare cases it
+# isn't needed but there is little reason not to have the empty subclass.)
+#
+# One instance of the LangIntel class will be created for each codeintel
+# language. Code browser functionality and some buffer functionality
+# often defers to the LangIntel singleton.
+#
+# This is especially important for multi-lang files. For example, an
+# HTML buffer uses the JavaScriptLangIntel and the CSSLangIntel for
+# handling codeintel functionality in <script> and <style> tags.
+#
+# See other lang_*.py files in your Komodo installation for examples of
+# usage.
+class MakoLangIntel(LangIntel):
+    lang = lang
+
+#---- Buffer class
+
+class MakoBuffer(UDLBuffer, XMLParsingBufferMixin):
+    lang = lang
+    tpl_lang = lang
+    m_lang = "HTML"
+    css_lang = "CSS"
+    csl_lang = "JavaScript"
+    ssl_lang = "Python"
+
+    cpln_stop_chars = "'\" (;},~`@#%^&*()=+{}]|\\;,.<>?/"
+
+#---- CILE Driver class
+
+# Dev Notes:
+# A CILE (Code Intelligence Language Engine) is the code that scans
+# Mako content and returns a description of the code in that file.
+# See "cile_mako.py" for more details.
+#
+# The CILE Driver is a class that calls this CILE. If Mako is
+# multi-lang (i.e. can contain sections of different language content,
+# e.g. HTML can contain markup, JavaScript and CSS), then you will need
+# to also implement "scan_multilang()".
+#class MakoCILEDriver(CILEDriver):
+#    lang = lang
+#
+#    def scan_purelang(self, buf):
+#        import cile_mako
+#        return cile_mako.scan_buf(buf)
+
+class MakoCILEDriver(UDLCILEDriver):
+    lang = lang
+    csl_lang = "JavaScript"
+    ssl_lang = "Python"
+
+#---- registration
+
+def register(mgr):
+    """Register language support with the Manager."""
+    mgr.set_lang_info(
+        lang,
+        silvercity_lexer=MakoLexer(),
+        buf_class=MakoBuffer,
+        langintel_class=MakoLangIntel,
+        import_handler_class=None,
+        cile_driver_class=MakoCILEDriver,
+        is_cpln_lang=True)
